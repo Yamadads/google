59f3917487a8458cb0e33b60f9cc8bc6c9be9799
Yamadads <pawlaksebastian@hotmail.com>
2017-10-09 11:35:43 +0200

    app

---
+++ program/App.py
@@ -0,0 +1,19 @@
+
+class App:
+    documents = []
+    terms = []
+
+    def load_documents(self, filename):
+        print (filename)
+
+    def load_terms(self, filename):
+        print (filename)
+
+    def get_transformed_documents(self):
+        print("documents")
+
+    def get_transformed_terms(self):
+        print("terms")
+
+    def query(self, query):
+        print("list of documents")
